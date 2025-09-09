011aba2715a11ef23cd51a54efb3c33d35c76597
Shubham Patil <spworkspace.10@gmail.com>
2025-09-09 01:59:16 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'el

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: 'Store-Safe',
+  title: 'CipherSafe',
   description: 'Decentralized Password Security',
 };
 
